cc289a9fa6206d947a9c785683db4824539d67c1
senthilmurugan <senthilmurugan4321@gmail.com>
2022-12-13 18:35:29 +0530

    initial commit

---
+++ back-end/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.function.Consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
